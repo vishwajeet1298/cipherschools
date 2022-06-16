be041fcf2ef7767db16f14dc0e7508ce7df25b27
Vishwajeet Singh <vishwajeet1298@gmail.com>
2022-06-16 18:59:02 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,4 @@
+const taskInput = document.querySelector(".task-input input"),
+filters = document.querySelectorAll(".filters span"),
+clearAll = document.querySelector(".clear-btn"),
+taskBox = document.querySelector(".task-box");
